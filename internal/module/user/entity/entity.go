34aea711a82e411c058abf1600f389c80d26d260
hilmiikhsan <ikhsanhilmimuhammad@gmail.com>
2024-08-31 16:18:24 +0700

    feat: add validation min and max name field

--- internal/module/user/entity/entity.go
+++ internal/module/user/entity/entity.go
@@ -1,7 +1,7 @@
 package entity
 
 type RegisterRequest struct {
-	Name     string `json:"name" validate:"required"`
+	Name     string `json:"name" validate:"required,min=3,max=50"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 
